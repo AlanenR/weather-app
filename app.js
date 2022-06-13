7e7617a133271dfaeedd9472407b398444a12802
Riikka Alanen <69243343+AlanenR@users.noreply.github.com>
2022-06-13 11:47:29 -0500

    Added icon and html tags

--- app.js
+++ app.js
@@ -13,11 +13,14 @@ app.get("/", function(req, res){
            const weatherData = JSON.parse(data);
            const temp = weatherData.main.temp;
            const weatherDescription = weatherData.weather[0].description;
-
-           console.log("Temperature is " + temp + " and description is " + weatherDescription);
+           const icon = weatherData.weather[0].icon;
+           const iconUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+           res.write("<p>The weather is currently " + weatherDescription + "</p>");
+           res.write("<h1>Temperature is " + temp + " degrees celcius.</h1>");
+           res.write("<img src=" + iconUrl + ">");
+           res.send();
         });
     });
-    res.send("Server is up and running");
 });
 
 
