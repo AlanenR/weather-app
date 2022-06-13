e7c18acafb9d5a0b74cbb836d023b44778fe9b6e
Riikka Alanen <69243343+AlanenR@users.noreply.github.com>
2022-06-13 12:37:37 -0500

    Connected form input

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const https = require("https");
-const key =  // insert API key from https://openweathermap.org
+const bodyParser = require('body-parser');
+
+const key = // insert API key from https://openweathermap.org
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", function(req, res){
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Helsinki&appid=" + key + "&units=metric";
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+    const city = req.body.cityName;
+    const unit = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key + "&units=" + unit;
 
     https.get(url, function(response){
         console.log(response.statusCode);
@@ -16,14 +26,15 @@ app.get("/", function(req, res){
            const icon = weatherData.weather[0].icon;
            const iconUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
            res.write("<p>The weather is currently " + weatherDescription + "</p>");
-           res.write("<h1>Temperature is " + temp + " degrees celcius.</h1>");
+           res.write("<h1>Temperature in " + city + " is " + temp + " degrees celcius.</h1>");
            res.write("<img src=" + iconUrl + ">");
            res.send();
         });
     });
-});
+  });
 
 
 app.listen(3000, function (){
     console.log("Server is running on port 3000");
 });
+
